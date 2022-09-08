7e35689bf1dfe6ac81bee45f11c3825f97caa6b9
sunzhengyu99 <sunzy9901@qq.com>
2022-09-08 22:50:22 +0800

    test2更新

--- demo/src/demo.java
+++ demo/src/demo.java
@@ -1,6 +1,7 @@
 public class demo {
     public static void main(String[] args) {
         System.out.println("test1 update");
+        System.out.println("test2 update");
         System.out.println("hello world!");
     }
 }
