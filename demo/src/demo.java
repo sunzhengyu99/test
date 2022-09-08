196291af865cec60808047298a734925c00652ea
sunzhengyu99 <sunzy9901@qq.com>
2022-09-08 23:02:40 +0800

    test2更新

--- demo/src/demo.java
+++ demo/src/demo.java
@@ -2,9 +2,16 @@ public class demo {
     public static void main(String[] args) {
         System.out.println("test1 update");
         System.out.println("test1 update");
+        System.out.println("test2 update");
+        System.out.println("hello world!");
+        System.out.println("hello world!");
         System.out.println("hello world!");
     }
     public void helle(){
+        System.out.println("test1");
+    }
+
+    public void helle1(){
         System.out.println("test1");
     }
 }
